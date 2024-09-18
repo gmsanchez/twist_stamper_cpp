70a52e49e0d0d2daa88d0622d39a923c44b10020
Guido Sanchez <sanchez.guido@gmail.com>
2024-09-18 13:17:30 +0000

    Add launch file with remappings.

---
+++ launch/twist_stamper_cpp_remapped_launch.py
@@ -0,0 +1,27 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+from launch_ros.descriptions import ParameterValue
+
+def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    node_robot_state_publisher = Node(
+        package = 'twist_stamper_cpp',
+        executable = 'twist_stamper_cpp',
+        output = 'screen',
+        parameters=[{'frame_id': 'base_link'}, {'use_sim_time': use_sim_time}],
+        remappings=[('/cmd_vel_out','/hibachi_base_controller/cmd_vel'),
+                    ('/cmd_vel_in', '/hibachi_base_controller/cmd_vel_unstamped')]
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
+        node_robot_state_publisher,
+    ])
